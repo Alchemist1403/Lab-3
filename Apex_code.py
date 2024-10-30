bee64552992d27733e1fd64d6703135f540b6946
Alchemist <Alchemist1403@yandex.ru>
2024-10-30 22:23:32 +0300

    Добавил комментарии

--- Apex_code.py
+++ Apex_code.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from tkinter import messagebox
 
 
-
+# Списки для работы функции, генерирующей ключ
 DOUBLE_LATIN_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
 DOUBLE_NUMBERS = "01234567890123456789"
 SYMBOLS = "012345678ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -23,8 +23,9 @@ def create_key(a):
     main_key = ""
     key_creation = ""
 
+    # Первый блок ключа
     for row in range(5):
-        key_creation += random.choice(SYMBOLS)  # Первый блок ключа
+        key_creation += random.choice(SYMBOLS)
     main_key += key_creation
 
     for other_blocks in range(3):
@@ -32,33 +33,37 @@ def create_key(a):
         key_creation = list(key_creation)
         key_creation.pop(random.randint(0,len(key_creation)-1))
 
+        # Второй блок ключа
         if len(key_creation) == 4:
             for symbol in range(len(key_creation)):
                 if key_creation[symbol] in DOUBLE_LATIN_ALPHABET:
                     key_creation[symbol] = DOUBLE_LATIN_ALPHABET[DOUBLE_LATIN_ALPHABET.index(key_creation[symbol])+int(a[0])]
                 if key_creation[symbol] in DOUBLE_NUMBERS:
-                    key_creation[symbol] = DOUBLE_NUMBERS[DOUBLE_NUMBERS.index(key_creation[symbol])+int(a[0])] # Второй блок ключа
+                    key_creation[symbol] = DOUBLE_NUMBERS[DOUBLE_NUMBERS.index(key_creation[symbol])+int(a[0])]
 
+        # Третий блок ключа
         if len(key_creation) == 3:
             for symbol in range(len(key_creation)):
                 if key_creation[symbol] in DOUBLE_LATIN_ALPHABET:
                     key_creation[symbol] = DOUBLE_LATIN_ALPHABET[DOUBLE_LATIN_ALPHABET.index(key_creation[symbol])-int(a[1])]
                 if key_creation[symbol] in DOUBLE_NUMBERS:
-                    key_creation[symbol] = DOUBLE_NUMBERS[DOUBLE_NUMBERS.index(key_creation[symbol])-int(a[1])] # Третий блок ключа
-
+                    key_creation[symbol] = DOUBLE_NUMBERS[DOUBLE_NUMBERS.index(key_creation[symbol])-int(a[1])]
+        
+        # Четвёртый блок ключа
         if len(key_creation) == 2:
             for symbol in range(len(key_creation)):
                 if key_creation[symbol] in DOUBLE_LATIN_ALPHABET:
                     key_creation[symbol] = DOUBLE_LATIN_ALPHABET[DOUBLE_LATIN_ALPHABET.index(key_creation[symbol])+int(a[2])]
                 if key_creation[symbol] in DOUBLE_NUMBERS:
-                    key_creation[symbol] = DOUBLE_NUMBERS[DOUBLE_NUMBERS.index(key_creation[symbol])+int(a[2])] # Четвёртый блок ключа
-
+                    key_creation[symbol] = DOUBLE_NUMBERS[DOUBLE_NUMBERS.index(key_creation[symbol])+int(a[2])]
         key_creation = "".join(key_creation)
         main_key += f'-{key_creation}'
 
     return main_key
 
+
 def click():
+    '''Функция выводит сгенерированный ключ доступа после нажатия кнопки'''
     number = number_entry.get()
     if len(number) != 3 or number == "000":
         tk.messagebox.showwarning('Ошибка','Это не трёхзначное число!')
@@ -87,19 +92,18 @@ def click():
 
 
 
-
+# Запуск программы и размеры
 root = tk.Tk()
-root.title('Генератора ключа')          # Запуск программы и размеры
+root.title('Генератора ключа')
 root.geometry('1300x800')
 
 
-
+# Фон
 bg_image = tk.PhotoImage(file='Crypto.png')
-label_bg = tk.Label(root, image=bg_image)            # Фон
+label_bg = tk.Label(root, image=bg_image)
 label_bg.place(x=0, y=0, relwidth=1, relheight=1)
 
-
-
+# Верхняя надпись "Введите трёхзначное число"
 input_number_label = tk.Label(
     root,
     text='Введите трёхзначное число ',
@@ -111,11 +115,11 @@ input_number_label = tk.Label(
 )
 input_number_label.place(x=30,y=140)
 
-
+# Окно для ввода числа
 number_entry = tk.Entry(root, width=5,borderwidth=7, font=('Arial_Black', 50))
 number_entry.place(x=60, y=270)
 
-
+# Надпись "Сгенерированный ключ"
 key_creation_label = tk.Label(
     root,
     text='Сгенерированный ключ ',
@@ -125,9 +129,9 @@ key_creation_label = tk.Label(
     highlightbackground="#282828",
     highlightthickness=3
 )
-
 key_creation_label.place(x=30,y=450)
 
+# Кнопка рядом с вводом числа
 btn_key = tk.Button(
     root,
     text = "OK",
